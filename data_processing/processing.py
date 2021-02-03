544073d582c54df400ac1a8ce19d6ba5f677f8dd
dbsousa01 <danielbsousa1@gmail.com>
2021-02-03 20:20:13 +0000

    Build output function

--- data_processing/processing.py
+++ data_processing/processing.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 # File variables
@@ -14,3 +15,24 @@ def load_data():
     df = pd.read_csv(file_path, index_col="observation_id")
 
     return df
+
+
+def build_outcome_label(df: pd.DataFrame) -> pd.DataFrame:
+    """
+        Created a new column in the dataframe that is the outcome of the search -> successful or not
+
+    :param df: DataFrame to be processed
+    :return: df with the new label column
+    """
+
+    df_label = df.copy()
+    sucessful_outcomes = ["Local resolution", "Community resolution", "Offender given drugs possession warning",
+                          "Khat or Cannabis warning", "Caution (simple or conditional)",
+                          "Offender given penalty notice", "Arrest", "Penalty Notice for Disorder",
+                          "Suspected psychoactive substances seized - No further action", "Summons / charged by post",
+                          "Article found - Detailed outcome unavailable", "Offender cautioned", "Suspect arrested",
+                          "Suspect summonsed to court"]
+
+    df_label["label"] = df_label["Outcome"]
+    df_label["label"] = np.where(df_label["Outcome"].isin(sucessful_outcomes), 1, 0)
+    return df_label
