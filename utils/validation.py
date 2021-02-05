8d91e40e3a478e977b4368be3c44afcba170159d
dbsousa01 <danielbsousa1@gmail.com>
2021-02-05 17:20:02 +0000

    Function to validate object

---
+++ utils/validation.py
@@ -0,0 +1,19 @@
+"""
+    Functions to validate the input json
+"""
+
+
+def check_request(request):
+    """
+        Validates that our request is well formatted
+
+        Returns:
+        - assertion value: True if request is ok, False otherwise
+        - error message: empty if request is ok, False otherwise
+    """
+
+    if "observation_id" not in request:
+        error = "Field `observation_id` missing from request: {}".format(request)
+        return False, error
+
+    return True, ""
