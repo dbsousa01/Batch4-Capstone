30d98d26035582f58b59c25fd68da6ecc6205448
dbsousa01 <danielbsousa1@gmail.com>
2021-02-05 10:42:17 +0000

    Feature importance function

--- utils/modelling.py
+++ utils/modelling.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+
+from typing import List
+from datetime import date
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 
 
@@ -42,3 +47,34 @@ def create_train_test(
     y_test = test[["label"]]
 
     return X_train, X_test, y_train.to_numpy(), y_test.to_numpy()
+
+
+def feature_importance(feature_names: List[str], clf: Pipeline, save_path: str) -> None:
+    """
+        Function to create a plot that orders features and their importances for the trained model.
+    Currently it's only implemented for LinearSVC since it is the best performing model
+
+    :param feature_names: Name of features that are being used
+    :param clf: The classifier object
+    :param save_path: Path of the file for the image to be saved
+    :return:
+    """
+
+    # clf name
+    classifier_name = clf.named_steps["classifier"].__class__.__name__
+
+    # today date
+    today_date = date.today().strftime("%d%m%Y")
+
+    # named_steps to get the classifier from the Pipeline object
+    imp = clf.named_steps["classifier"].feature_importances_
+    imp, names = zip(*sorted(zip(imp, feature_names)))
+
+    plt.barh(range(len(names)), imp, align="center")
+    plt.yticks(range(len(names)), names)
+    plt.tight_layout()
+    plt.savefig(
+        save_path + "{0}_feature_importance_{1}.png".format(classifier_name, today_date)
+    )
+    plt.show()
+    return
