f7ee5daa36770a28bea381a0853f741a58083d7a
dbsousa01 <danielbsousa1@gmail.com>
2021-02-11 21:58:37 +0000

    Check valid columns in new requests

--- utils/validation.py
+++ utils/validation.py
@@ -17,3 +17,38 @@ def check_request(request):
         return False, error
 
     return True, ""
+
+
+def check_valid_column(observation):
+    """
+        Validates that our observation has the minimum needed columns
+
+    :param observation: json object
+    :return:
+        - assertion value: True if all provided columns are valid, False otherwise
+        - error message: empty if all provided columns are valid, False otherwise
+    """
+
+    valid_columns = {
+        "observation_id",
+        "Type",
+        "Date",
+        "Part of a policing operation",
+        "Latitude",
+        "Longitude",
+        "Gender",
+        "Age range",
+        "Officer-defined ethnicity",
+        "Legislation",
+        "Object of search",
+        "station",
+    }
+
+    keys = set(observation.keys())
+
+    if len(valid_columns - keys) > 0:
+        missing = valid_columns - keys
+        error = "Missing columns: {}".format(missing)
+        return False, error
+
+    return True, ""
